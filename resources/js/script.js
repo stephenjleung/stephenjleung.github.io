8edeeef39e91d38a64f2b687934bae7672e115e8
steponahen <steponahen@yahoo.com>
2016-05-21 23:16:47 +0000

    Connected with BehindTheName api and added a random baby name generator upon page refresh

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,21 @@
-console.log("testing js file link");
+//baby name api key st918764
+
+var apiLink = "http://www.behindthename.com/api/";
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (xhttp.readyState == 4 && xhttp.status == 200) {
+     // Action to be performed when the document is read;
+     //console.log(xhttp.responseText);
+     var data = xhttp.responseXML;
+     
+     var x = data.getElementsByTagName("name");
+     console.log(x);
+     console.log(x[0].textContent);
+     var firstName = x[0].textContent;
+     document.getElementById("random-name").innerHTML = firstName;
+  }
+};
+
+xhttp.open("GET", "http://www.behindthename.com/api/random.php?usage=eng&key=st918764", true);
+xhttp.send();
