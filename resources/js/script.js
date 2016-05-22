1ed769190fb079c83b395aa58bb9d3fae1423ab8
steponahen <steponahen@yahoo.com>
2016-05-22 00:25:09 +0000

    Added a button to click to generate a list of 6 different baby names.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,21 +1,49 @@
 //baby name api key st918764
 
-var apiLink = "http://www.behindthename.com/api/";
 
+window.onload = function(){ 
+    
+
+var apiLink = 'http://www.behindthename.com/api/';
+
+
+
+
+var getRandomNames = function(num) {
+  
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (xhttp.readyState == 4 && xhttp.status == 200) {
-     // Action to be performed when the document is read;
-     //console.log(xhttp.responseText);
-     var data = xhttp.responseXML;
+    // Action to be performed when the document is read;
+    //console.log(xhttp.responseText);
+    var data = xhttp.responseXML;
+    
+    var x = data.getElementsByTagName('name');
+    console.log(x.length);
+    console.log(x[0].textContent);
+    var firstName = '';
+    
+    //Empties the list of random names
+    var node = document.getElementById('random-name');
+      while (node.hasChildNodes()) {
+        node.removeChild(node.firstChild);
+      }
+    
+    for (var i = 0; i < x.length; i++) {
+      firstName = x[i].textContent;
+      document.getElementById('random-name').insertAdjacentHTML('beforeend', firstName + '</br>');
+    }
      
-     var x = data.getElementsByTagName("name");
-     console.log(x);
-     console.log(x[0].textContent);
-     var firstName = x[0].textContent;
-     document.getElementById("random-name").innerHTML = firstName;
   }
 };
 
-xhttp.open("GET", "http://www.behindthename.com/api/random.php?usage=eng&key=st918764", true);
+xhttp.open('GET', 'http://www.behindthename.com/api/random.php?usage=eng&number=' + num + '&key=st918764', true);
 xhttp.send();
+
+}
+document.getElementById("random-button").onclick = function() {getRandomNames(6)};
+
+
+//getRandomNames(6);
+
+};
