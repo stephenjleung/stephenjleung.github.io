492f5b6709a703ae45eb0a3f8bf51c39db65fb79
steponahen <steponahen@yahoo.com>
2016-06-02 00:42:59 +0000

    Added modal button to display copy-friendly favorites list

--- resources/js/script.js
+++ resources/js/script.js
@@ -465,7 +465,42 @@
     // Populate favorites list on page load
     updateFavorites();
     
-    // Populate initial random names (default filters)
-    //getRandomNames("m","eng",6);
+    
+    
+    // Modal controls
+    // Get the modal
+    var modal = document.getElementById('export-modal');
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("export-modal-button");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-modal")[0];
+    
+    var content = document.getElementById("export-favorites-list");
+    
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+      
+      emptyElementById("export-favorites-list");
+      
+      for (var i = 0; i < favorites.length; i++) {
+        content.insertAdjacentHTML("beforeend", "<li>" + favorites[i] + "</li>");
+      }
+      
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
     
   };
